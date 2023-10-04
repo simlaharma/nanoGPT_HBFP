aa05f3872ee6db9b093731c089eda6bce3c62d14
simlaharma <simlaharma@gmail.com>
2023-10-05 03:38:21 +0900

    outofmem

--- train.py
+++ train.py
@@ -266,6 +266,8 @@ while True:
     if iter_num % eval_interval == 0 and master_process:
         losses = estimate_loss()
         print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        with open(log_file, 'a') as f:
+            print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}",file=f)
         if wandb_log:
             wandb.log({
                 "iter": iter_num,
@@ -330,6 +332,8 @@ while True:
             mfu = raw_model.estimate_mfu(batch_size * gradient_accumulation_steps, dt)
             running_mfu = mfu if running_mfu == -1.0 else 0.9*running_mfu + 0.1*mfu
         print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%")
+        with open(log_file, 'a') as f:
+            print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms, mfu {running_mfu*100:.2f}%",file=f)
     iter_num += 1
     local_iter_num += 1
 
