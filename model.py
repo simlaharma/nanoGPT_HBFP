ed7c3944c448e408a3f8290659fdad6744a22d32
simlaharma <simlaharma@gmail.com>
2023-10-05 00:03:42 +0900

    optim

--- model.py
+++ model.py
@@ -280,7 +280,7 @@ class GPT(nn.Module):
         print(f"num decayed parameter tensors: {len(decay_params)}, with {num_decay_params:,} parameters")
         print(f"num non-decayed parameter tensors: {len(nodecay_params)}, with {num_nodecay_params:,} parameters")
         # Create AdamW optimizer and use the fused version if it is available
-        fused_available = 'fused' in inspect.signature(torch.optim.AdamW).parameters
+        fused_available = False ##'fused' in inspect.signature(torch.optim.AdamW).parameters
         use_fused = fused_available and device_type == 'cuda'
         extra_args = dict(fused=True) if use_fused else dict()
         optimizer = BFPAdamW(optim_groups, lr=learning_rate, betas=betas, **extra_args)
